78aaa218bc79d03e87138f38cf09251a662faa5e
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-03-05 09:14:10 -0700

    Testing out internetcat

--- src/data/controller/Controller.java
+++ src/data/controller/Controller.java
@@ -1,5 +1,7 @@
 package data.controller;
 
+import data.model.InternetCat;
+
 public class Controller
 {
 	public Controller()
@@ -9,6 +11,11 @@ public class Controller
 	
 	public void start()
 	{
+		String[] tags = {"cute", "cuddly"};
+		InternetCat demo = new InternetCat(tags, "image/jpeg", 123, "", "", "garbage");
 		
+		System.out.println(demo);
+		
+		System.out.println("Here is the createdAt value " + demo.createdAt());
 	}
 }
