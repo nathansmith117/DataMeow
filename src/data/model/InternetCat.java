5e68dd9711d7c95c50b976123903c12830fc84c6
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-03-05 09:50:26 -0700

    more constructor thingy thing

--- src/data/model/InternetCat.java
+++ src/data/model/InternetCat.java
@@ -2,5 +2,8 @@ package data.model;
 
 public record InternetCat(String[] tags, String mimeType, int size, String createdAt, String editedAt, String _id)
 {
-
+	public InternetCat()
+	{
+		this(new String[] {}, "", 0, "", "", "");
+	}
 }
