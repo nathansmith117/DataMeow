f11fc9d8c7dba86321114d41c98e848a1c768cb1
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-03-07 07:54:27 -0700

    json thingy thing lmao

--- src/data/model/InternetCat.java
+++ src/data/model/InternetCat.java
@@ -1,5 +1,8 @@
 package data.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record InternetCat(String[] tags, String mimeType, int size, String createdAt, String editedAt, String _id)
 {
 	public InternetCat()
